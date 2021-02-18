9063d26aa7a35f73ed51a58cec54e95d5bf64e8b
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-02-17 22:30:45 -0300

    Lesson part three (full)

---
+++ lib/screens/book-form.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class BookForm extends StatefulWidget {
+  @override
+  _BookFormState createState() => _BookFormState();
+}
+
+class _BookFormState extends State<BookForm> {
+  final _formKey = GlobalKey<FormBuilderState>();
+  bool isRead = false;
+
+  void toggleSwitch(_) {
+    setState(() {
+      isRead = _formKey.currentState.fields['isRead'].value;
+
+      if (!isRead) {
+        _formKey.currentState.fields['rate']?.didChange(0.0);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Novo Livro")),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(25.0),
+        child: Container(
+          child: Column(
+            children: [
+              FormBuilder(
+                key: _formKey,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                child: Column(
+                  children: [
+                    FormBuilderTextField(
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(context,
+                            errorText: "O nome do livro é obrigatório."),
+                        FormBuilderValidators.minLength(context, 3,
+                            errorText:
+                                "O nome do livro precisa ter pelo menos 3 caracteres.")
+                      ]),
+                      name: "title",
+                      decoration: InputDecoration(
+                          labelText: "Título do Livro",
+                          hintText: "Título",
+                          border: OutlineInputBorder()),
+                      keyboardType: TextInputType.name,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    FormBuilderTextField(
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(context,
+                            errorText: "O nome do autor é obrigatório."),
+                        FormBuilderValidators.minLength(context, 3,
+                            errorText:
+                                "O nome do autor precisa ter pelo menos 3 caracteres.")
+                      ]),
+                      name: "author",
+                      decoration: InputDecoration(
+                          labelText: "Nome do Autor",
+                          hintText: "Nome",
+                          border: OutlineInputBorder()),
+                      keyboardType: TextInputType.name,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    FormBuilderSwitch(
+                      name: "isRead",
+                      initialValue: false,
+                      title: Text("Leitura finalizada?"),
+                      onChanged: toggleSwitch,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    isRead
+                        ? FormBuilderRating(
+                            name: 'rate',
+                            initialValue: 0.0,
+                            max: 5.0,
+                            enabled: isRead,
+                            validator: FormBuilderValidators.compose([
+                              FormBuilderValidators.required(context),
+                              FormBuilderValidators.min(context, 1.0,
+                                  errorText:
+                                      "Deixe uma avaliação para o livro!")
+                            ]))
+                        : Container()
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              RaisedButton(
+                child: Text("Salvar"),
+                onPressed: () {
+                  _formKey.currentState.validate();
+                  print(_formKey.currentState.value);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:power_library/models/book.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Book> _myBooks = [
+    Book(author: "Josias", title: "Universo em Desencanto", isRead: true),
+    Book(
+        author: "Josias",
+        title: "Universo em Desencanto parte 2",
+        isRead: true),
+    Book(author: "Manolo", title: "Java para Iniciantes", isRead: false),
+    Book(
+        author: "Manolo",
+        title: "Estrutura de Dados para Iniciantes",
+        isRead: true),
+    Book(author: "Manolo", title: "Arquitetura de Software", isRead: false)
+  ];
+
+  int readBooksCount() {
+    return _myBooks.where((book) => book.isRead).length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isIOS = Theme.of(context).platform == TargetPlatform.iOS;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: Text("Power Library")),
+      body: Container(
+        margin: EdgeInsets.only(top: 20.0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment
+          //     .center, // essa linha diz: alinha o main axis (y) pro centro em relação ao teu elemento-pai.
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.asset(
+                "assets/images/logowide.png",
+                width: 240.0,
+              ),
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemCount: _myBooks.length,
+                itemBuilder: (ctx, index) => ListTile(
+                  title: Text(_myBooks[index].title),
+                  subtitle: Text(_myBooks[index].author),
+                  leading: Icon(Icons.book),
+                  trailing: _myBooks[index].isRead
+                      ? Icon(Icons.check_box_outlined)
+                      : Icon(Icons.check_box_outline_blank),
+                ),
+                separatorBuilder: (context, index) => Divider(),
+              ),
+            ),
+            Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.fromLTRB(
+                    20.0, isIOS ? 20.0 : 10.0, 20.0, isIOS ? 22.0 : 10.0),
+                decoration: BoxDecoration(color: Colors.teal),
+                child: Text(
+                    "Livros Lidos: ${readBooksCount()}/${_myBooks.length}",
+                    style: TextStyle(color: Colors.white)))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {Navigator.pushNamed(context, '/book-form')},
+        tooltip: "Adicionar Livro",
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
