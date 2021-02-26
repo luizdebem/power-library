b747b055d79d60820452afa5b36e966e641c095c
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-02-26 14:41:53 -0300

    Extras: added validation on signup/signin forms

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -21,82 +21,108 @@ class _SigninScreenState extends State<SigninScreen> {
       appBar: AppBar(
         title: Text("Faça login na PowerLibrary"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FormBuilder(
-                    key: _formKey,
-                    autovalidateMode: AutovalidateMode.disabled,
-                    child: Padding(
-                      padding: const EdgeInsets.all(50),
-                      child: Column(
-                        children: [
-                          FormBuilderTextField(
-                            name: "email",
-                            decoration: InputDecoration(
-                                labelText: "E-mail",
-                                border: OutlineInputBorder()),
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          Divider(),
-                          FormBuilderTextField(
-                            obscureText: true,
-                            name: "password",
-                            decoration: InputDecoration(
-                                labelText: "Senha",
-                                border: OutlineInputBorder()),
-                            keyboardType: TextInputType.text,
-                          ),
-                          Divider(),
-                          FlatButton(
-                            onPressed: () async {
-                              _formKey.currentState.save();
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FormBuilder(
+                      key: _formKey,
+                      child: Padding(
+                        padding: const EdgeInsets.all(50),
+                        child: Column(
+                          children: [
+                            FormBuilderTextField(
+                              validator: FormBuilderValidators.compose([
+                                FormBuilderValidators.required(context,
+                                    errorText: "Campo obrigatório."),
+                                FormBuilderValidators.email(context,
+                                    errorText: "Deve ser um e-mail válido.")
+                              ]),
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              name: "email",
+                              decoration: InputDecoration(
+                                  labelText: "E-mail",
+                                  border: OutlineInputBorder()),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            Divider(),
+                            FormBuilderTextField(
+                              validator: FormBuilderValidators.compose([
+                                FormBuilderValidators.required(context,
+                                    errorText: "Campo obrigatório."),
+                                FormBuilderValidators.minLength(context, 8,
+                                    errorText: "Mínimo 8 caracteres."),
+                                FormBuilderValidators.match(context,
+                                    r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$',
+                                    errorText:
+                                        "Deve conter números, caracteres especiais e maiúsculos.")
+                              ]),
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              obscureText: true,
+                              name: "password",
+                              decoration: InputDecoration(
+                                  labelText: "Senha",
+                                  border: OutlineInputBorder()),
+                              keyboardType: TextInputType.text,
+                            ),
+                            Divider(),
+                            FlatButton(
+                              onPressed: () async {
+                                if (_formKey.currentState.validate()) {
+                                  _formKey.currentState.save();
 
-                              final email =
-                                  _formKey.currentState.value['email'];
-                              final password =
-                                  _formKey.currentState.value['password'];
-                              print(email);
-                              print(password);
-                              await AuthService()
-                                  .signInWithEmailAndPassword(email, password);
-                              Navigator.of(context).popUntil((route) {
-                                return route.settings.name == "/";
-                              });
-                            },
-                            color: Colors.amber,
-                            textColor: Colors.black,
-                            padding: EdgeInsets.all(8.0),
-                            splashColor: Colors.amberAccent,
-                            child: Text("Entrar!"),
-                          ),
-                          Divider(
-                            height: 50.0,
-                          ),
-                          Text("Não possui uma conta ainda?"),
-                          FlatButton(
-                            onPressed: () {
-                              Navigator.pushNamed(
-                                  context, SignupScreen.routeName);
-                            },
-                            color: Colors.white,
-                            textColor: Colors.black,
-                            padding: EdgeInsets.all(8.0),
-                            splashColor: Colors.amberAccent,
-                            child: Text("Crie uma conta."),
-                          ),
-                        ],
+                                  final email =
+                                      _formKey.currentState.value['email'];
+                                  final password =
+                                      _formKey.currentState.value['password'];
+                                  print(email);
+                                  print(password);
+                                  await AuthService()
+                                      .signInWithEmailAndPassword(
+                                          email, password);
+                                  Navigator.of(context).popUntil((route) {
+                                    return route.settings.name == "/";
+                                  });
+                                } else {
+                                  return;
+                                }
+                              },
+                              color: Colors.amber,
+                              textColor: Colors.black,
+                              padding: EdgeInsets.all(8.0),
+                              splashColor: Colors.amberAccent,
+                              child: Text("Entrar!"),
+                            ),
+                            Divider(
+                              height: 50.0,
+                            ),
+                            Text("Não possui uma conta ainda?"),
+                            FlatButton(
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                    context, SignupScreen.routeName);
+                              },
+                              color: Colors.white,
+                              textColor: Colors.black,
+                              padding: EdgeInsets.all(8.0),
+                              splashColor: Colors.amberAccent,
+                              child: Text("Crie uma conta."),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -19,69 +19,96 @@ class _SignupScreenState extends State<SignupScreen> {
       appBar: AppBar(
         title: Text("Crie sua conta"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Column(
-              children: [
-                FormBuilder(
-                  key: _formKey,
-                  autovalidateMode: AutovalidateMode.disabled,
-                  child: Padding(
-                    padding: const EdgeInsets.all(50),
-                    child: Column(
-                      children: [
-                        FormBuilderTextField(
-                          name: "email",
-                          decoration: InputDecoration(
-                              labelText: "E-mail",
-                              border: OutlineInputBorder()),
-                          keyboardType: TextInputType.emailAddress,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    FormBuilder(
+                      key: _formKey,
+                      child: Padding(
+                        padding: const EdgeInsets.all(50),
+                        child: Column(
+                          children: [
+                            FormBuilderTextField(
+                              validator: FormBuilderValidators.compose([
+                                FormBuilderValidators.required(context,
+                                    errorText: "Campo obrigatório."),
+                                FormBuilderValidators.email(context,
+                                    errorText: "Deve ser um e-mail válido.")
+                              ]),
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              name: "email",
+                              decoration: InputDecoration(
+                                  labelText: "E-mail",
+                                  border: OutlineInputBorder()),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            Divider(),
+                            FormBuilderTextField(
+                              validator: FormBuilderValidators.compose([
+                                FormBuilderValidators.required(context,
+                                    errorText: "Campo obrigatório."),
+                                FormBuilderValidators.minLength(context, 8,
+                                    errorText: "Mínimo 8 caracteres."),
+                                FormBuilderValidators.match(context,
+                                    r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$',
+                                    errorText:
+                                        "Deve conter números, caracteres especiais e maiúsculos.")
+                              ]),
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              obscureText: true,
+                              name: "password",
+                              decoration: InputDecoration(
+                                  labelText: "Senha",
+                                  border: OutlineInputBorder()),
+                              keyboardType: TextInputType.text,
+                            ),
+                            Divider(),
+                            FlatButton(
+                              onPressed: () async {
+                                _formKey.currentState.save();
+                                if (_formKey.currentState.validate()) {
+                                  final email =
+                                      _formKey.currentState.value['email'];
+                                  final password =
+                                      _formKey.currentState.value['password'];
+                                  print(email);
+                                  print(password);
+                                  await AuthService()
+                                      .signUpWithEmailAndPassword(
+                                          email, password);
+                                  Navigator.of(context).popUntil((route) {
+                                    return route.settings.name == "/";
+                                  });
+                                } else {
+                                  return;
+                                }
+                              },
+                              color: Colors.amber,
+                              textColor: Colors.black,
+                              padding: EdgeInsets.all(8.0),
+                              splashColor: Colors.amberAccent,
+                              child: Text("Registrar!"),
+                            ),
+                            Divider(
+                              height: 50.0,
+                            ),
+                          ],
                         ),
-                        Divider(),
-                        FormBuilderTextField(
-                          obscureText: true,
-                          name: "password",
-                          decoration: InputDecoration(
-                              labelText: "Senha", border: OutlineInputBorder()),
-                          keyboardType: TextInputType.text,
-                        ),
-                        Divider(),
-                        FlatButton(
-                          onPressed: () async {
-                            _formKey.currentState.save();
-                            if (_formKey.currentState.validate()) {
-                              final email =
-                                  _formKey.currentState.value['email'];
-                              final password =
-                                  _formKey.currentState.value['password'];
-                              print(email);
-                              print(password);
-                              await AuthService()
-                                  .signUpWithEmailAndPassword(email, password);
-                              Navigator.of(context).popUntil((route) {
-                                return route.settings.name == "/";
-                              });
-                            } else {}
-                          },
-                          color: Colors.amber,
-                          textColor: Colors.black,
-                          padding: EdgeInsets.all(8.0),
-                          splashColor: Colors.amberAccent,
-                          child: Text("Registrar!"),
-                        ),
-                        Divider(
-                          height: 50.0,
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
